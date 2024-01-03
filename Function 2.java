26861403048d804700caa345189807e39679c515
Yasaswini Desai <ty4959@srmist.edu.in>
2024-01-04 00:01:44 +0530

    Function 2.java

--- Function 2.java
+++ Function 2.java
@@ -62,3 +62,52 @@ public class Main{
         System.out.println(binomialcoefficient(n,r));
     } 
 }
+
+//Binary to Decimal Code  
+
+import java.util.*;
+import java.lang.Math;
+public class Main {
+    public static void binary(int n) {
+        int decimal=0;
+        int i=0;
+        while(n>0){
+            int r=n%10;
+            decimal=decimal+(r*(int)Math.pow(2,i));
+            i++;
+            n=n/10;
+        }
+        System.out.print("The decimal Value is\n"+decimal);
+    }
+    public static void main(String[] args){
+       System.out.println("Enter the Binary Value :");
+        Scanner sc= new Scanner(System.in);
+        int n=sc.nextInt();
+        binary(n);
+    }
+}
+
+//Decimal to Binary Code  
+
+import java.util.*;
+import java.lang.Math;
+public class Main {
+    public static void decimal(int n) {
+        int binary=0;
+        int i=0;
+        while(n>0){
+            int r=n%2;
+            binary=binary+r*(int)Math.pow(10,i);
+            i++;
+            n=n/2;
+        }
+        System.out.println(binary);
+    }
+    public static void main(String[] args){
+        Scanner sc= new Scanner(System.in);
+           System.out.println("The Binary Value From 1 to 10 is :");
+           for(int i=1;i<=10;i++){
+           decimal(i);
+           }
+    }
+}
